1409182c212df9d6a917df137a20a9e1d13284a2
Aviemoon <ondrakroutil@gmail.com>
2026-02-15 21:22:44 +0100

    hurtbox fix :3
    they didnt work properly turns out

--- Utility/hurtbox.gd
+++ Utility/hurtbox.gd
@@ -3,9 +3,12 @@ extends Area2D
 signal hurt(p_friendly, p_damage, p_angle, p_knockback)
 @export var friendly = false
 @export_enum('HitOnce', 'Cooldown', 'Disable') var hurtbox_type = 0 
+@onready var disable_timer: Timer = $disableTimer
 
-var hit_once_array:Array = []
+var hit_once_array: Array = []
+var collision_in_hurtbox: Array = []
 
+@export var hurt_area_cooldown: float = 0.5
 
 func _on_area_entered(area):
 	check_area(area)
@@ -16,14 +19,22 @@ func _on_body_entered(body: Node2D) -> void:
 
 func check_body(body):
 	if body is Spike and body.get('damage'):
+		if body not in collision_in_hurtbox:
+			collision_in_hurtbox.append(body)
 		var damage = body.get('damage')
 		var attacker_type = false
-		
+		print(collision_in_hurtbox)
 		hurt.emit(attacker_type, damage, 0, 0)
+		await get_tree().create_timer(hurt_area_cooldown).timeout
+		print(collision_in_hurtbox)
+		if body in collision_in_hurtbox:
+			check_body(body)
 
 
 func check_area(area):
 	if (area.is_in_group('attack_friendly') or area.is_in_group('attack_unfriendly')) and area.get('damage'):
+		if area not in collision_in_hurtbox:
+			collision_in_hurtbox.append(area)
 		match hurtbox_type:
 			0:
 				
@@ -33,7 +44,7 @@ func check_area(area):
 					return
 			1:
 				$CollisionShape2D.call_deferred('set', 'disabled', true)
-				$disableTimer.start()
+				disable_timer.start()
 			2:
 				pass
 		var angle = Vector2.ZERO
@@ -45,6 +56,9 @@ func check_area(area):
 		else:
 			attacker_type = false
 		hurt.emit(attacker_type, damage, angle, knockback)
+		await get_tree().create_timer(hurt_area_cooldown).timeout
+		if area in collision_in_hurtbox:
+			check_area(area)
 
 
 
@@ -55,3 +69,10 @@ func _on_disable_timer_timeout() -> void:
 func _on_area_exited(area: Area2D) -> void:
 	if area in hit_once_array:
 		hit_once_array.erase(area)
+		collision_in_hurtbox.erase(area)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body in collision_in_hurtbox:
+		
+		collision_in_hurtbox.erase(body)
